fd321109d89e0b8aab8fd201f01b1f2e3f59b0ed
Bohdan Levytskyi <levytskyibohdan@gmail.com>
2019-12-25 19:30:50 +0200

    dev_v2.0.3

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-$(document).ready(function () {
-    $(".navbar-toggler").on("click", function (event) {
-        const isShow = $(".header").attr('class').split(/\s+/).includes('show');
-        switch (isShow) {
-            case true: 
-                $(".header").addClass('hide');
-                $(".header").removeClass('show');
-                break;
-            case false: 
-                $(".header").addClass('show');
-                $(".header").removeClass('hide');
-                break;
-        }
-        console.log(isShow);
-    });
-    console.log('work')
-});
+const headerBackground = () => {
+    const header = document.querySelector('.header').classList;
+    switch (document.querySelector('.navbar-collapse').offsetHeight) {
+        case 0:
+            header.add('show');
+            header.remove('hide');
+            break;
+        case 299:
+            header.add('hide');
+            header.remove('show');
+    }
+}
+
+document.querySelector('.navbar-toggler').addEventListener('click', () => {
+    headerBackground();
+})
+
+document.querySelectorAll('.mobile .nav-item').forEach(item => item.addEventListener('click', ()=> {
+    headerBackground();
+}))
